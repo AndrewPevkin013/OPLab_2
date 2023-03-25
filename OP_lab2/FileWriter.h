1437c22742e4834418a7678fc1e33d83fc3765b8
AP_0013 <112945923+AndrewPevkin013@users.noreply.github.com>
2023-03-25 15:42:58 +0300

    Upload lab 2

---
+++ OP_lab2/FileWriter.h
@@ -0,0 +1,13 @@
+#pragma once
+#include <string>
+using namespace std;
+
+class FileWriter
+{
+public:
+
+  void createNewFile(int &n, string newFile);
+  void openFile(int& n);
+  void showFileData();
+};
+
