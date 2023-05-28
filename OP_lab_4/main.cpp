51ede34c484799e516c2e7e4295cd8c66f0d7da1
AP_0013 <112945923+AndrewPevkin013@users.noreply.github.com>
2023-05-28 15:44:09 +0300

    Update main.cpp

--- OP_lab_4/main.cpp
+++ OP_lab_4/main.cpp
@@ -4,11 +4,11 @@
 using namespace Figures;
 
 int main() {
-    FigureSystem fs;
-    std::cout << "Hello, World!" << std::endl;
-    fs.addTrapezium(6, 10, 20);
-    fs.PrintTrapezium();
-    fs.addLineSegment(1, 1, 4, 3);
-    fs.PrintLine();
+    //FigureSystem fs;
+    //std::cout << "Hello, World!" << std::endl;
+    //fs.addTrapezium(6, 10, 20);
+    //fs.PrintTrapezium();
+    //fs.addLineSegment(1, 1, 4, 3);
+    //fs.PrintLine();
     return 0;
 }
